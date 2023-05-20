5575f16850d8f188e8737e5925079510b4fd6e89
MarceloRemeseiro <marcelo.remeseiro@gmail.com>
2023-05-20 17:23:38 +0200

    home

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -3,10 +3,13 @@ import './App.css';
 
 function Hello() {
   return (
-    <div className="h-screen flex items-center justify-center bg-gray-200">
-      <h1 className="text-blue-500 text-5xl">
-        TEMPLATE-ELECTRON_REACT_TAILWIND
-      </h1>
+    <div className="h-screen flex justify-center bg-gray-200">
+      <div className="text-blue-500 text-center text-5xl">
+        <h1>TEMPLATE </h1>
+        <h1>ELECTRON </h1>
+        <h1>REACT</h1>
+        <h1>TAILWIND</h1>
+      </div>
     </div>
   );
 }
